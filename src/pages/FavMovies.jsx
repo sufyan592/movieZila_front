e1f4164313d3d3640abe4a1f96e820d6ff8fee4e
Sufyan <suuf786@gmail.com>
2024-01-15 22:18:14 +0500

    change

--- src/pages/FavMovies.jsx
+++ src/pages/FavMovies.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Nav from "../components/nav/Nav";
 import FaviroteMoviesComp from "../components/favirouteMovies/FaviroteMoviesComp";
-import MovieFooter from "../components/footer/MovieFooter";
 
 const FavMovies = () => {
   return (
